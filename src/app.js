477980e20082189547c89cde6a13e59ad6fe866e
Mary-7777 <maryna.mitroshyna@gmail.com>
2023-03-11 21:37:45 +0400

    Display the forecast data using the API

--- src/app.js
+++ src/app.js
@@ -23,28 +23,44 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Thu", "Fri", "Sat", "Sun"];
   let forecastHTML = `<div class="row">`;
-
-  days.forEach(function (day) {
+  days.forEach(function (forecastDay, index) {
+    if (index < 6) {
     forecastHTML =
       forecastHTML +
       `
           <div class="col-2">
-            <div class="weather-forecast-date">${day}</div>
-              <img src="http://openweathermap.org/img/wn/10n@2x.png" alt="" width="42" />
+            <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+            ${index}
+              <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" alt="" width="42" />
                 <div class="weather-forecast-temperatures">
+                
                 <span class="weather-forecast-temperature-max">
-                  18˚ </span>
+                  ${Math.round(
+                  forecastDay.temp.max
+                )}˚ </span>
+
                 <span class="weather-forecast-temperature-min">
-                  12˚</span>
+                  ${Math.round(
+                  forecastDay.temp.min
+                )}˚</span>
+
           </div>
       </div>
-  `;
+    `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
